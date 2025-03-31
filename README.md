a67abaa09766eb406ee51ae8fb3ac34579202817
Kyllian <kyllianvibancos2@gmail.com>
2025-03-31 13:50:03 +0200

    almost finished

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# intro-javascript
-# intro-javascript
+Étapes à suivre
+🔹 1. Déclaration de variables
+Crée une variable pour le nom de l'utilisateur
+
+Crée une variable pour son âge
+
+Crée une variable pour sa ville
+
+🔹 2. Réaffectation de valeur
+Change la valeur de la variable "ville" pour une autre ville
+
+🔹 3. Création d’un objet
+Crée un objet utilisateur qui regroupe les informations : nom, âge, ville
+
+🔹 4. Modification de propriété
+Change la ville dans l’objet utilisateur
+
+🔹 5. Ajout de propriété
+Ajoute une propriété email à l’objet utilisateur
+
+🔹 6. Création d’un tableau
+Ajoute une propriété amis à l’objet utilisateur, qui contient une liste de prénoms
+
+🔹 7. Manipulation du tableau
+Supprime un ami de la liste
+
+Ajoute un nouvel ami à la fin de la liste
+
+Remplace un ami par un autre
+
+🔹 8. Parcours du tableau
+Utilise une boucle pour afficher chaque ami dans la console
+
+🔹 9. Conditions
+Si l’utilisateur a moins de 18 ans, affiche “Mineur”
+
+S’il a entre 18 et 25 ans, affiche “Jeune adulte”
+
+Sinon, affiche “Adulte”
+
+🔹 10. Fonctions (nouvelle section)
+a) Fonction simple
+Crée une fonction saluerUtilisateur qui prend un prénom et affiche un message de bienvenue
+
+b) Fonction avec retour
+Crée une fonction calculerAnneeNaissance qui prend l’âge en paramètre et retourne l’année de naissance (en supposant qu’on est en 2025)
+
+c) Fonction avec condition
+Crée une fonction verifierMajorite qui prend un âge en paramètre et retourne true si l’utilisateur est majeur, sinon false
+
+d) Fonction qui manipule un objet
+Crée une fonction afficherProfil qui prend l’objet utilisateur et affiche toutes ses infos dans un message formaté
+
+e) Bonus :
+Crée une fonction ajouterAmi qui ajoute un prénom dans le tableau amis de l’objet utilisateur
+
+Crée une fonction supprimerAmi qui supprime un prénom s’il existe
---
+++ index.js
@@ -0,0 +1,101 @@
+//etape 1
+const firstName = "Neo"
+const age = 25
+let ville = "Matrix"
+
+//etape 2
+ville = "New York"
+
+//etape 3
+const utilisateur = {firstName, age, ville}
+
+//etape 4
+utilisateur.ville = "Los Angeles"
+
+//etape 5
+utilisateur.email = "neo@gmail.com"
+
+//etape 6
+utilisateur.amis = ["Morpheus", "Trinity", "Smith"]
+
+//etape 7
+utilisateur.amis.pop("Smith")
+
+//etape 7.1
+utilisateur.amis.push("Wick")
+
+//etape 7.2
+utilisateur.amis.splice(2, 1, "John")
+
+//etape 8
+//TODO : à refaire
+for(let i = 0; i<utilisateur.amis.length; i++){
+    console.log(utilisateur.amis.length);
+}
+
+//étape 9
+if (utilisateur.age < 18){
+    console.log("Mineur");
+} else if (utilisateur.age < 25){
+    console.log("Jeune adulte");
+    
+} else {
+    console.log("Adulte");
+    
+}
+
+//étape 10 a)
+const saluerUtilisateur = () => {
+    console.log("Hello", utilisateur.firstName);
+    
+}
+
+saluerUtilisateur()
+
+//étape 10 b)
+const calculerAnneeNaissance = (annee, age) =>{
+    return annee - age
+
+}
+
+console.log("calcul annee naissance", calculerAnneeNaissance(2025, utilisateur.age))
+
+//étape 10 c)
+const verifierMajorite = (age) =>{
+if (utilisateur.age > age) {
+    return true
+} return false
+}
+
+console.log("vérifie majorité",verifierMajorite( 18));
+
+//étape 10 d)
+const afficherProfil = () =>{
+console.log("Bonjjour", utilisateur.firstName, "vous avez", utilisateur.age, "ans et vivez à", utilisateur.ville);
+( utilisateur)
+}
+
+afficherProfil()
+
+//étape 10 e) bonus
+const ajouterAmi = (nom) =>{
+    return utilisateur.amis.push(nom)
+}
+console.log(ajouterAmi("Jean"));
+console.log(utilisateur.amis);
+
+
+//bonus 2
+// const supprimerAmi = (firstName) =>{
+//     if (utilisateur.amis${firstName}){
+// console.log(utilisateur.amis(firstName))
+//     } else {
+//         console.log("FirstName doesn't exist");
+        
+//     }
+// }
+
+// supprimerAmi("Morpheus")
+// console.log(utilisateur);
+
+
